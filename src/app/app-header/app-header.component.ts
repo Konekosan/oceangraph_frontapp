70fc0cf9be2280f1e82c934a38596876a37ef337
Espeletta <haverlan.lola@hotmail.fr>
2024-09-11 17:45:32 +0200

    ajout utilisateur

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MaterialModule } from 'src/_module/Material.Module';
+import { UsersComponent } from '../users/users.component';
 
 @Component({
   selector: 'app-app-header',
   standalone: true,
-  imports: [MaterialModule, RouterLink],
+  imports: [MaterialModule, RouterLink, UsersComponent],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,9 @@ export const routes: Routes = [
     {
         path: 'boards',
         loadComponent: () => import('./board/board.component').then(m => m.BoardComponent)
+    },
+    {
+        path: 'users',
+        component: UsersComponent
     }
 ];
